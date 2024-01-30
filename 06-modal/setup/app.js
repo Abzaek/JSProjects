19e45e864a9f769888cb3d4ea9d7179ca765d00e
Abdulazez <abzaek@hotmail.com>
2024-01-30 16:22:10 +0300

    added JS to 06-modal

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -2,3 +2,16 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalBtn = document.querySelector('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+
+[modalBtn, closeBtn].forEach((element) => element.addEventListener('click', function (e) {
+    if (e.target == modalBtn) {
+        modalOverlay.classList.add('open-modal');
+    } else {
+        modalOverlay.classList.remove('open-modal');
+    };
+}));
